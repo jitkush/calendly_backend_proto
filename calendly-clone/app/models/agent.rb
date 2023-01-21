2db006ca8b974ba108b778f92b02fd0317b7aeae
jartek <2030949+jartek@users.noreply.github.com>
2023-01-21 20:06:51 +0530

    refactor: simplify create logic for the controller

--- calendly-clone/app/models/agent.rb
+++ calendly-clone/app/models/agent.rb
@@ -1,16 +1,20 @@
 class Agent < ApplicationRecord
-    has_many :meeting
+  has_many :meetings
 
-    email_pattern = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
-    text = /[a-zA-Z]+/i
+  email_pattern = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+  text = /[a-zA-Z]+/i
 
-    #validating name, cannot be null and should contain string only
-    validates :name, 
-    format: {with: text, message: "Please enter text"},
-    presence: {message: "Name cannot be blank" }
-    
-    #validaing email, cannot be null, should adher to the emal format, cannot be duplicate
-    validates :email, presence: {message: "Email cannot be blank" },
-    uniqueness: {message: "Email already present, please login or recover"},
-    format: {with: email_pattern, message: "invalid email"}
+  # validating name, cannot be null and should contain string only
+  validates :name,
+            format: { with: text, message: 'Please enter text' },
+            presence: { message: 'Name cannot be blank' }
+
+  # validaing email, cannot be null, should adher to the emal format, cannot be duplicate
+  validates :email, presence: { message: 'Email cannot be blank' },
+                    uniqueness: { message: 'Email already present, please login or recover' },
+                    format: { with: email_pattern, message: 'invalid email' }
+
+  def available?(date, time)
+    meetings.exists?(date: date, time: time)
+  end
 end
--- calendly-clone/config/routes.rb
+++ calendly-clone/config/routes.rb
@@ -4,21 +4,21 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  #agent routes
+  # agent routes
   get 'agent/list', to: 'agent#list'
   post 'agent/add', to: 'agent#add'
   patch 'agent/update/:id', to: 'agent#update'
   get 'agent/show/:id', to: 'agent#show'
   delete 'agent/remove/:id', to: 'agent#remove_agent'
 
-  #client routes
+  # client routes
   get 'client/list', to: 'client#list'
   post 'client/add', to: 'client#add'
   patch 'client/update/:id', to: 'client#update'
   get 'client/show/:id', to: 'client#show'
   delete 'client/remove/:id', to: 'client#remove_client'
 
-  #meeting routes
+  # meeting routes
   post '/meeting/add/', to: 'meeting#add_meeting'
   get '/meeting/all', to: 'meeting#all_meeting'
   get 'meeting/agent/upcoming/:id', to: 'meeting#view_agent_meeting'
